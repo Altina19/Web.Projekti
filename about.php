ffae57106080e1e48e94641ab49be2b7fbe5e18f
Altina19 <103149537+Altina19@users.noreply.github.com>
2022-04-24 18:39:12 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="css/about.css">
+    <title>ALEN FASHION</title>
+</head>
+<body>
+
+    <nav>
+        <div class="logo">
+            <h4>ALEN Fashion</h4>
+        </div>
+        <ul class="nav-links">
+        <li><a href="indexweb.php">HOME</a></li>
+        &emsp;
+        <li><a href="new.php">NEW</a></li>
+        &emsp;
+        <li><a href="indexcart.php">ACCESSORIES</a></li>
+        &emsp;
+        <li><a href="about.php">ABOUT</a></li>
+        &emsp;
+        <li><a href="indexlog.php">LOGIN</a></li>
+        &emsp;
+    </ul>
+    <div class="menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+    </nav>
+    
+    <section id="hero">
+        
+      </section>
+    
+      <section id="prodetails" class="section-p1">
+          <div class="single-pro-image">
+              <img src="img/dresss.png" width="100%" id="MainImg">
+    
+          <div class="small-img-group">
+              <div class="small-img-col">
+                  <img src="img/dresss1.png" width="100%" class="small-img">
+              </div>
+              <div class="small-img-col">
+                  <img src="img/dresss2.png" width="100%" class="small-img">
+              </div>
+              <div class="small-img-col">
+                  <img src="img/dresss3.png" width="100%" class="small-img">
+              </div>
+             </div>
+          </div>
+          <div class="single-pro-details">
+              <h2>Pershkrimi</h2>
+              <h5>HEDDA RED DRESS</h5>
+              <h2>45  €</h2>
+              <select>
+                  <option>Zgjedh masen</option>
+                  <option>XL</option>
+                  <option>XXL</option>
+                  <option>L</option>
+                  <option>M</option>
+                  <option>S</option>
+              </select>
+              <input type="number" value="1">
+              <button class="btn"><a href="indexcart.php" style="color:white;">Shiko me shume</a></button>
+              <h4>HARLEE BUBBLE GUM PINK DRESS</h4>
+              <span>Fustani ‘Hedda’ është bërë në mënyrë perfekte nga materiali më i lëmuar i satenit me printe shumëngjyrëshe. <br>
+                  Me detaje për lidhjen më të bukur në bel dhe shumë i rehatshëm.<br> 
+                  Kombinohet perfekt me taka transparente apo edhe atlete</span>
+                   <h4>Materiali: 100% POLYESTER<br>
+    
+                      Detajet: Printe shumëngjyrëshe<br>
+                      
+                      Përkujdesja: Pastrim në makinë larëse në temperaturë te ulët</h4>
+          </div>
+      </section>
+    
+      <section id="product1" class="section-p1">
+        <h2>New Collection</h2>
+        <div class="pro-container">
+            <div class="pro"onclick="window.location.href='about.html';">
+                <img src="img/longdress9.png">
+                <div class="des">
+                    <span>Dress</span>
+                    <h5>Beautiful prints on our pieces</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>69€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/longdress1.png">
+                <div class="des">
+                    <span>Dress</span>
+                    <h5>Figure perfection and feminine vibes</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>65€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/longdress2.png">
+                <div class="des">
+                    <span>Dress</span>
+                    <h5>Show a glimpse of skin in our 'Anne'Beige Multi Dress</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>79€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/longdress3.png">
+                <div class="des">
+                    <span>Corset Tops</span>
+                    <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>42€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/longdress14.png">
+                <div class="des">
+                    <span>Corset Tops</span>
+                    <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>42€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/longdress5.png">
+                <div class="des">
+                    <span>Corset Tops</span>
+                    <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>42€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/longdress6.png">
+                <div class="des">
+                    <span>Corset Tops</span>
+                    <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>42€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+            <div class="pro">
+                <img src="img/longdress7.png">
+                <div class="des">
+                    <span>Corset Tops</span>
+                    <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+                    <div class="star">
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                      <i class="bx bx-star"></i>
+                    </div>
+                    <h4>42€</h4>
+                </div>
+                <a href="#"><i class="bx bx-cart cart"></i></a>
+            </div>
+    </div>
+    </section>
+
+    <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Faqet</h4>
+  	 			<ul>
+  	 				<li><a href="indexcart.php">Accessories</a></li>
+  	 				<li><a href="indexweb.php">Home</a></li>
+  	 				<li><a href="new.php">New</a></li>
+  	 				<li><a href="indexlog.php">Login</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Me shume:</h4>
+                   <p>Standard Shipping: Kosovo (24 - 48 hours), FREE. 
+                       <br>
+                       Albania (2 - 3 business days), €4.00. 
+                       <br>
+                       Macedonia (3-5 business days), €5.00.  </p>  
+                                                  
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Produktet</h4>
+  	 			<ul>
+  	 				<li><a href="indexcart.php">Dresses</a></li>
+  	 				<li><a href="index-page3.php">Bags</a></li>
+  	 				<li><a href="index-page2.php">Shoes</a></li>
+  	 				<li><a href="new.php">News</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+    
+                <script src="js/about.js"></script>
+</body>
+</html>

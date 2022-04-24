783ec15b8a944301d9a559c05885c277b1b8d2ee
Altina19 <103149537+Altina19@users.noreply.github.com>
2022-04-24 18:53:47 +0200

    php3

---
+++ php3/header-page3.php
@@ -0,0 +1,70 @@
+
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="indexweb.php" class="navbar-brand">
+            <h3 class="px-5">
+                <i class="fas fa-shopping-basket"></i> ALEN FASHION
+            </h3>
+        </a>
+        <button class="navbar-toggler"
+            type="button"
+                data-toggle="collapse"
+                data-target = "#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <nav>
+        
+        <ul class="nav-links">
+        <li><a href="indexweb.php">HOME</a></li>
+        &emsp;
+        <li><a href="new.php">NEW</a></li>
+        &emsp;
+        <li><a href="indexcart.php">ACCESSORIES</a></li>
+        &emsp;
+        <li><a href="about.php">ABOUT</a></li>
+        &emsp;
+        <li><a href="indexlog.php">LOGIN</a></li>
+        &emsp;
+    </ul>
+    <div class="menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+    </nav>
+    <link rel="stylesheet" href="css/accessories.php">
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="mr-auto"></div>
+            <div class="navbar-nav">
+                <a href="carta-page3.php" class="nav-item nav-link active">
+                    <h5 class="px-5 cart">
+                        <i class="fas fa-shopping-cart"></i> Cart
+                        <?php
+
+                        if (isset($_SESSION['cart'])){
+                            $count = count($_SESSION['cart']);
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
+                        }else{
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
+                        }
+
+                        ?>
+                    </h5>
+                </a>
+            </div>
+        </div>
+
+    </nav>
+</header>
+
+
+
+
+
+

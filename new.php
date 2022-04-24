dbb6ac70d2c900a70c4e2220cae98c9325fc7141
Altina19 <103149537+Altina19@users.noreply.github.com>
2022-04-24 18:42:23 +0200

    new.php

---
+++ new.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/new.css" >
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
+       integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+       <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+
+    <nav>
+        <div class="logo">
+            <h4>ALEN Fashion</h4>
+        </div>
+        <ul class="nav-links">
+        <li><a href="indexweb.php">HOME</a></li>
+        &emsp;
+        <li><a href="new.php">NEW</a></li>
+        &emsp;
+        <li><a href="indexcart.php">ACCESSORIES</a></li>
+        &emsp;
+        <li><a href="about.php">ABOUT</a></li>
+        &emsp;
+        <li><a href="indexlog.php">LOGIN</a></li>
+        &emsp;
+    </ul>
+    <div class="menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+    </nav>
+
+    <section id="hero">
+
+    </section>
+
+
+    <section id="banner" class="section-p1">
+        <div class="banner-box">
+            <h4>Clothing</h4>
+            <button class="white">Shiko me shume</button>
+        </div>
+        <div class="banner-box banner-box2">
+            <h4>Dresses</h4>
+            <button class="white">Shiko me shume</button>
+        </div>
+    </section>
+
+    <section id="banner2">
+        <div class="banner-box">
+            <h2>Sezona e zbritjeve</h2>
+            <h3>Zbritje deri ne 30%</h3>
+        </div>
+        <div class="banner-box banner-box3">
+            <h2>Sezona e zbritjeve</h2>
+            <h3>Zbritje deri ne 30%</h3>
+        </div>
+        <div class="banner-box banner-box4">
+            <h2>Sezona e zbritjeve</h2>
+            <h3>Zbritje deri ne 30%</h3>
+        </div>
+    </section>
+
+
+    <section id="product1" class="section-p1">
+      <h2>Fashion Style</h2>
+      <p>Summer Collection New</p>
+      <div class="pro-container">
+          <div class="pro"onclick="window.location.href='about.php';">
+              <img src="img/dresss.png">
+              <div class="des">
+                  <span>ALEN Fashion</span>
+                  <h5>HARLEE BUBBLE GUM PINK DRESS</h5>
+                  <div class="star">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                  </div>
+                  <h4>65€</h4>
+              </div>
+              <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+          </div>
+          <div class="pro">
+              <img src="img/sets2.png">
+              <div class="des">
+                  <span>ALEN Fashion</span>
+                  <h5>ANAYA ORANGE DRESS</h5>
+                  <div class="star">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                  </div>
+                  <h4>159€</h4>
+              </div>
+              <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+          </div>
+          <div class="pro">
+              <img src="img/sets6.png">
+              <div class="des">
+                  <span>ALEN Fashion</span>
+                  <h5>KILEY BROWN DRESS</h5>
+                  <div class="star">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                  </div>
+                  <h4>99€</h4>
+              </div>
+              <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+          </div>
+          <div class="pro">
+              <img src="img/longdress12.png">
+              <div class="des">
+                  <span>Corset Tops</span>
+                  <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+                  <div class="star">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                  </div>
+                  <h4>42€</h4>
+              </div>
+              <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+          </div>
+          <div class="pro">
+            <img src="img/longdress13.png">
+            <div class="des">
+                <span>Corset Tops</span>
+                <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h4>42€</h4>
+            </div>
+            <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+        </div>
+        <div class="pro">
+          <img src="img/sets.png">
+          <div class="des">
+              <span>Corset Tops</span>
+              <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+              <div class="star">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+              </div>
+              <h4>42€</h4>
+          </div>
+          <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+      </div>
+      <div class="pro">
+        <img src="img/sets4.png">
+        <div class="des">
+            <span>Corset Tops</span>
+            <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+            <div class="star">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <h4>42€</h4>
+        </div>
+        <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+    </div>
+    <div class="pro">
+      <img src="img/shortdress.png">
+      <div class="des">
+          <span>Corset Tops</span>
+          <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+          <div class="star">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+          </div>
+          <h4>42€</h4>
+      </div>
+      <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+  </div>
+  <div class="pro">
+    <img src="img/shortdress1.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+<div class="pro">
+    <img src="img/shortdress2.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+<div class="pro">
+    <img src="img/shortdress3.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+<div class="pro">
+    <img src="img/shortdress4.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+<div class="pro">
+    <img src="img/shortdress5.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+<div class="pro">
+    <img src="img/shortdress6.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+<div class="pro">
+    <img src="img/shortdress7.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+<div class="pro">
+    <img src="img/shortdress8.png">
+    <div class="des">
+        <span>Corset Tops</span>
+        <h5>The 'Kenzie'Coffe Top.Pair it with our 'Margo'Chocolate Pants for a complete look</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <h4>42€</h4>
+    </div>
+    <a href="indexcart.php"><i class="fal fa-shopping-cart cart"></i></a>
+</div>
+        </div>
+      </section>
+
+      
+      <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Faqet</h4>
+  	 			<ul>
+  	 				<li><a href="indexcart.php">Accessories</a></li>
+  	 				<li><a href="indexweb.php">Home</a></li>
+  	 				<li><a href="new.php">New</a></li>
+  	 				<li><a href="indexlog.php">Login</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Me shume:</h4>
+                   <p>Standard Shipping: Kosovo (24 - 48 hours), FREE. 
+                       <br>
+                       Albania (2 - 3 business days), €4.00. 
+                       <br>
+                       Macedonia (3-5 business days), €5.00.  </p>  
+                                                  
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Produktet</h4>
+  	 			<ul>
+  	 				<li><a href="indexcart.php">Dresses</a></li>
+  	 				<li><a href="index-page3.php">Bags</a></li>
+  	 				<li><a href="index-page2.php">Shoes</a></li>
+  	 				<li><a href="new.php">News</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+    
+
+            
+  <script src="js/indexweb.js"></script>
+</body>
+</html>

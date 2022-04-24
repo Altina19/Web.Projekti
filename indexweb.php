55acd97cea44e0b7945d80fb44f3df90a4b27ae2
Altina19 <103149537+Altina19@users.noreply.github.com>
2022-04-24 18:41:43 +0200

    indexweb.php

---
+++ indexweb.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
+       integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+       <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="css/accessories.css">
+    <link rel="stylesheet" type="text/css" href="css/indexweb.css">
+    <title>ALEN FASHION</title>
+</head>
+<body>
+
+<nav>
+        <div class="logo">
+            <h4>ALEN Fashion</h4>
+        </div>
+        <ul class="nav-links">
+        <li><a href="indexweb.php">HOME</a></li>
+        &emsp;
+        <li><a href="new.php">NEW</a></li>
+        &emsp;
+        <li><a href="indexcart.php">ACCESSORIES</a></li>
+        &emsp;
+        <li><a href="about.php">ABOUT</a></li>
+        &emsp;
+        <li><a href="indexlog.php">LOGIN</a></li>
+        &emsp;
+    </ul>
+    <div class="menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+    </nav>
+
+    <div class="hero">
+        <h4>Fashion Style</h4>
+        <h2>Style your look</h2>
+        <h1>On all products</h1>
+        <button><a href="indexcart.php">Shop Now</a></button>
+    </div>
+
+    <main>
+        <div class="text">
+            <h1>Fashion Style</h1>
+            <p>Time to Shine</p>
+        </div>
+        <header>
+            <p>
+                <span>&#139;</span>
+                <span>&#155;</span>
+            </p>
+        </header>
+        <section>
+            <div class="product">
+                <picture>
+                    <img src="img/sets2.png">
+                </picture>
+                <div class="details">
+                    <p>
+                        <b>Kemisha</b>
+                        <small>New arrival</small>
+                    </p>
+                    <samp>15.00</samp>
+                </div>
+                <div class="butoni">
+                    <p class="star">
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                    </p>
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+            <div class="product">
+                <picture>
+                    <img src="img/jeans10.png">
+                </picture>
+                <div class="details">
+                    <p>
+                        <b>Pantollona-Bluze-Cizme-Cante</b>
+                        <small>New arrival</small>
+                    </p>
+                    <samp>45.00</samp>
+                </div>
+                <div class="butoni">
+                    <p class="star">
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                    </p>
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+            <div class="product">
+                <picture>
+                    <img src="img/new8.png">
+                </picture>
+                <div class="details">
+                    <p>
+                        <b>Fustani</b>
+                        <small>New arrival</small>
+                    </p>
+                    <samp>35.00</samp>
+                </div>
+                <div class="butoni">
+                    <p class="star">
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                    </p>
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+            <div class="product">
+                <picture>
+                    <img src="img/new9.png">
+                </picture>
+                <div class="details">
+                    <p>
+                        <b>Pantollona-Kemish-Pallte-Cante</b>
+                        <small>New arrival</small>
+                    </p>
+                    <samp>70.00</samp>
+                </div>
+                <div class="butoni">
+                    <p class="star">
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                    </p>
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+            <div class="product">
+                <picture>
+                    <img src="img/pantolla.png">
+                </picture>
+                <div class="details">
+                    <p>
+                        <b>Fustane-Cizme-Cante</b>
+                        <small>New arrival</small>
+                    </p>
+                    <samp>45.00</samp>
+                </div>
+                <div class="butoni">
+                    <p class="star">
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                        <strong>&star;</strong>
+                    </p>
+                    <a href="#">Buy</a>
+                </div>
+            </div>
+            <div class="product">
+              <picture>
+                  <img src="img/photo3.png">
+              </picture>
+              <div class="details">
+                  <p>
+                      <b>Pantollona-Bluze-Cizme-Cante</b>
+                      <small>New arrival</small>
+                  </p>
+                  <samp>45.00</samp>
+              </div>
+              <div class="butoni">
+                  <p class="star">
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                  </p>
+                  <a href="#">Buy</a>
+              </div>
+          </div>
+          <div class="product">
+              <picture>
+                  <img src="img/photo7.png">
+              </picture>
+              <div class="details">
+                  <p>
+                      <b>Pantollona-Bluze-Cizme-Cante</b>
+                      <small>New arrival</small>
+                  </p>
+                  <samp>45.00</samp>
+              </div>
+              <div class="butoni">
+                  <p class="star">
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                  </p>
+                  <a href="#">Buy</a>
+              </div>
+          </div>
+          <div class="product">
+              <picture>
+                  <img src="img/sets7.png">
+              </picture>
+              <div class="details">
+                  <p>
+                      <b>Pantollona-Bluze-Cizme-Cante</b>
+                      <small>New arrival</small>
+                  </p>
+                  <samp>45.00</samp>
+              </div>
+              <div class="butoni">
+                  <p class="star">
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                      <strong>&star;</strong>
+                  </p>
+                  <a href="#">Buy</a>
+              </div>
+          </div>
+          
+        </section>
+    </main> 
+    
+
+    <div class="profile">
+        <div class="instruction">
+            <a href="new.html">
+                <img src="img/shortdress6.png">
+                <h3>Dress</h3>
+                <div class="filter"></div>
+            </a>
+        </div>
+        <div class="instruction">
+            <a href="accessories.html">
+                <img src="img/shoes7.png">
+                <h3>Shoes</h3>
+                <div class="filter"></div>
+            </a>
+        </div>
+        <div class="instruction">
+            <a href="page2.html">
+                <img src="img/bags4.png">
+                <h3>Bags</h3>
+                <div class="filter"></div>
+            </a>
+        </div>
+    </div>
+
+    <div id="feature" class="section-p1">
+        <div class="fe-box">
+            <img src="img/download (1).png">
+            <h6>Transport 48H</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/images (1).png">
+            <h6>Blerje Online</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/images.png">
+            <h6>24h vonese</h6>
+        </div>
+        <div class="fe-box">
+            <img src="img/pay.png">
+            <h6>Paguaj me kartel</h6>
+        </div>
+    </div>
+
+    
+          
+
+                <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Faqet</h4>
+  	 			<ul>
+  	 				<li><a href="indexcart.php">Accessories</a></li>
+  	 				<li><a href="indexweb.php">Home</a></li>
+  	 				<li><a href="new.php">New</a></li>
+  	 				<li><a href="indexlog.php">Login</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Me shume:</h4>
+                   <p>Standard Shipping: Kosovo (24 - 48 hours), FREE. 
+                       <br>
+                       Albania (2 - 3 business days), €4.00. 
+                       <br>
+                       Macedonia (3-5 business days), €5.00.  </p>  
+                                                  
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Produktet</h4>
+  	 			<ul>
+  	 				<li><a href="indexcart.php">Dresses</a></li>
+  	 				<li><a href="index-page3.php">Bags</a></li>
+  	 				<li><a href="index-page2.php">Shoes</a></li>
+  	 				<li><a href="new.php">News</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+    
+    <script src="js/indexweb.js"></script>
+</body>
+</html>

d1c15f6ba48da6b596a0e93f4319b931a0320b8f
Altina19 <103149537+Altina19@users.noreply.github.com>
2022-04-24 18:47:10 +0200

    index-page3.php

---
+++ index-page3.php
@@ -0,0 +1,138 @@
+<?php
+
+session_start();
+
+require_once ('php3/CreateDb-page3.php');
+require_once ('./php3/component-page3.php');
+
+
+// create instance of Createdb class
+$database = new CreateDb("Productcb", "Users");
+
+if (isset($_POST['add'])){
+    /// print_r($_POST['product_id']);
+    if(isset($_SESSION['cart'])){
+
+        $item_array_id = array_column($_SESSION['cart'], "product_id");
+
+        if(in_array($_POST['product_id'], $item_array_id)){
+            echo "<script>alert('Product is already added in the cart..!')</script>";
+            echo "<script>window.location = 'index-page3.php'</script>";
+        }else{
+
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'product_id' => $_POST['product_id']
+            );
+
+            $_SESSION['cart'][$count] = $item_array;
+        }
+
+    }else{
+
+        $item_array = array(
+                'product_id' => $_POST['product_id']
+        );
+
+        // Create new session variable
+        $_SESSION['cart'][0] = $item_array;
+        print_r($_SESSION['cart']);
+    }
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shopping Cart</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    
+    <link rel="stylesheet" href="style-page2.css">
+    <link rel="stylesheet" href="css/accessories.css">
+</head>
+<body>
+
+
+<?php require_once ("php3/header-page3.php"); ?>
+<div class="container">
+        <div class="row text-center py-5">
+            <?php
+                $result = $database->getData();
+                while ($row = mysqli_fetch_assoc($result)){
+                    component($row['product_name'], $row['product_price'], $row['product_image'], $row['id']);
+                }
+            ?>
+        </div>
+</div>
+
+
+<section id="numbers" class="section-p1">
+            <a href="indexcart.php">1</a>
+            <a href="index-page2.php">2</a>
+            <a href="index-page3.php">3</a>
+        </section>
+
+        <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Faqet</h4>
+  	 			<ul>
+  	 				<li><a href="accessories.php">Accessories</a></li>
+  	 				<li><a href="indexweb.php">Home</a></li>
+  	 				<li><a href="new.php">New</a></li>
+  	 				<li><a href="indexlog.php">Login</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Me shume:</h4>
+                   <p>Standard Shipping: Kosovo (24 - 48 hours), FREE. 
+                       <br>
+                       Albania (2 - 3 business days), €4.00. 
+                       <br>
+                       Macedonia (3-5 business days), €5.00.  </p>  
+                                                  
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Produktet</h4>
+  	 			<ul>
+  	 				<li><a href="indexcart.php">Dresses</a></li>
+  	 				<li><a href="index-page3.php">Bags</a></li>
+  	 				<li><a href="index-page2.php">Shoes</a></li>
+  	 				<li><a href="new.php">News</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
